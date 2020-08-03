c08f6087654fd8f7aa6f6da50820f361bd06db54
TheJediCode <68616265+TheJediCode@users.noreply.github.com>
2020-08-03 11:15:57 -0400

    Initial commit for Wayfair thing

---
+++ wayfair.js
@@ -0,0 +1,25 @@
+let h = 89;
+let w = 36;
+let d = 18;
+let V = (w * d) * h;
+
+console.log(`${V} in cubed`);
+
+let Vair = V / 61.024;
+console.log(`${Vair} liters`);
+
+let O = 550;
+
+let o = Vair / O;
+
+console.log(o)
+
+let oDay = 11000;
+let oHour = 11000 / 24;
+console.log(oHour);
+
+console.log(o * 0.94);
+
+let carbon = 500 / 24;
+
+console.log(carbon);
